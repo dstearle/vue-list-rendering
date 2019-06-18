ca2c47d8df8d62a91f67a51cec20cce8e73a61f7
dstearle <dallas.st.earle@gmail.com>
2019-06-17 17:20:23 -0700

    Created file for new list

---
+++ src/components/lists/ListEight.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="listOne">
+        <body>
+            <div class="container py-5">
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="n in 5" v-bind:key="n">{{ 'InGen List ' + n }}</li>
+                </ul>
+            </div>
+        </body>
+    </div>
+</template>
