d4f5ed266c6087e33a13b8f9964edb6d1e050ef9
dstearle <dallas.st.earle@gmail.com>
2019-09-03 00:58:47 -0700

    Set width for ListSeven and added more comments

--- src/components/lists/ListSix.vue
+++ src/components/lists/ListSix.vue
@@ -1,31 +1,64 @@
 <template>
-    <div class="container">
+
+    <div class="container" style="width: 450px">
+
+        <!-- List Title -->
         <h2 class="py-5">Top 10 Jurassic Park Carnivores</h2>
+
         <p>The list below sorts and unsorted array from lowest score to highest...</p>
+
         <p>Click on "Score" to sort the list from ascending or descending order!</p>
+
+        <!-- Table -->
         <table class="table table-bordered table-striped">
+
+            <!-- Table Header -->
             <thead class="thead-dark">
+
                 <tr>
+                    <!-- Title For Table -->
                     <th>Carnivore</th>
+
+                    <!-- Toggle For Score -->
                     <th @click="toggle" :class="['sort-control', sortType]">Score</th>
+
                 </tr>
+
             </thead>
+
+            <!-- Table Body -->
             <tbody>
+
+                <!-- Table Rows -->
                 <tr v-for="predator of topTenCarnivoresSorted" v-bind:key="predator.name">
+
+                    <!-- Name -->
                     <td>{{ predator.name }}</td>
+
+                    <!-- Score -->
                     <td>{{ predator.score }}</td>
+
                 </tr>
+
             </tbody>
+            
         </table>
+
     </div>
+
 </template>
 
 <script>
 
     export default {
+
         data() {
+
             return {
+
+                // Array to be displayed in the list
                 topTenCarnivores: [
+
                     { name: "Ceratosaurus", score: 88 },
                     { name: "T-Rex", score: 100 },
                     { name: "Spinosaurus", score: 95 },
@@ -36,26 +69,45 @@
                     { name: "Metricanthosaurus", score: 89 },
                     { name: "Carnotaurus", score: 93 },
                     { name: "Acrocanthosaurus", score: 89}
+
                 ],
+
                 order: 1
+
                 }
+
         },
+
         computed: {
+
             // Orders the list in the topTenCarnivores array by score
             topTenCarnivoresSorted () {
+
                 return this.topTenCarnivores.sort((a, b) => (a.score - b.score) * this.order);
+
             },
+
             // Puts the sorted list into either an ascending or descending order
             sortType () {
+
                 return this.order === 1 ? 'ascending' : 'descending';
+
             }
+
         },
+
         methods: {
+
             // Toggles the list from ascending to descending order
+
             toggle () {
+
                 return this.order *= -1;
+
             }
+
         }
+
     }
 
 </script>
